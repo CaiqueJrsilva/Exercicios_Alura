2675dfec2a6c960bbb7b438ccc5ec5816abc17cb
Caique Junior da silva <139816879+CaiqueJrsilva@users.noreply.github.com>
2023-09-19 00:20:18 -0300

    adicionando mais exercicios

---
+++ testaCodicionais.java
@@ -0,0 +1,10 @@
+public class testaCodicionais {
+    public static void main(String[] args){
+     System.out.println("validando Idade ");
+     int idade = 24;
+     if(idade>=18){
+        System.out.println("Muito velho");
+     }
+    
+    }
+}
--- testeValores.java
+++ testeValores.java
@@ -1,10 +1,11 @@
-// public class testeValores {
-//      public static void main(String[] args){
-//             int primeiro = 5;
-//             int segundo = 7;
-//             segundo = primeiro;
-//             primeiro = 10;
-//             System.out.println(segundo);
-//         }
-// }
+public class testeValores {
+     public static void main(String[] args){
+            int primeiro = 5;
+            int segundo = 7;
+            segundo = primeiro;
+            primeiro = 10;
+            System.out.println(segundo);
+        }
+}
+
 
